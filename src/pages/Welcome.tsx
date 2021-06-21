28eefd2f6de23e9fcf9a6055f2d66afe09138097
enzoggqs <eggqsaraiva@gmail.com>
2021-06-21 19:56:54 -0300

    Refatorando codigo

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,18 @@
-import React, { useState} from 'react';
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { 
+    SafeAreaView, 
+    View, 
+    Text, 
+    Image, 
+    TouchableOpacity, 
+    StyleSheet,
+    Dimensions 
+} from 'react-native';
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
 
 export function Welcome(){
-  const [visible, setVisible] = useState(false);
-
-  function handleVisibility() {
-    setVisible(!visible)
-  }
-
   return(
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
@@ -20,17 +21,25 @@ export function Welcome(){
         de forma fácil
       </Text>
 
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image} />
-      }
+      <Image 
+        source={wateringImg} 
+        style={styles.image}
+        resizeMode="contain"
+      />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. 
         Nós cuidamos de lembrar semrpe que precisar 
       </Text>
 
-      <Button title=">"/>
+      <TouchableOpacity 
+        style={styles.button} 
+        activeOpacity={0.6}
+      >
+        <Text style={styles.buttonText}>
+          >
+        </Text>
+      </TouchableOpacity>
 
     </SafeAreaView>
   )
@@ -40,7 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   title: {
     fontSize: 32,
@@ -55,6 +64,9 @@ const styles = StyleSheet.create({
     padding: 20,
     color: colors.heading
   },
+  image: {
+    height: Dimensions.get('window').width * 0.7
+  },
   button: {
     backgroundColor: colors.green,
     justifyContent: 'center',
@@ -62,11 +74,7 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    paddingHorizontal: 10
-  },
-  image: {
-    width: 292,
-    height: 284
+    width: 56,
   },
   buttonText: {
     color: colors.white,
