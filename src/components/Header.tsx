03bfe3853c0a176d7ac36afaec66bb5f0ed822e3
enzoggqs <eggqsaraiva@gmail.com>
2021-06-22 12:02:28 -0300

    Criando componente cabeçalho

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import enzo from '../assets/enzo.jpg'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header(){
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>
+          Olá, 
+        </Text>
+        <Text style={styles.userName}>
+          Enzo
+        </Text>
+      </View>
+
+      <Image source={enzo} style={styles.image}/>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight() + 0.75,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName:{
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
+  },
+  image:{
+    width: 70,
+    height: 70,
+    borderRadius: 25,
+  },
+
+
+})
