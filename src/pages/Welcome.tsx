b64d4a9e30b48bdcbcf7b1779d2ab9f853ae13a6
enzoggqs <eggqsaraiva@gmail.com>
2021-06-21 23:07:23 -0300

    Atualizando a pagina Welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { 
     SafeAreaView, 
-    Text, 
+    Text,
+    View, 
     Image, 
     TouchableOpacity, 
     StyleSheet,
@@ -11,36 +12,40 @@ import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome(){
   return(
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
-
-      <Image 
-        source={wateringImg} 
-        style={styles.image}
-        resizeMode="contain"
-      />
-
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. 
-        Nós cuidamos de lembrar semrpe que precisar 
-      </Text>
-
-      <TouchableOpacity 
-        style={styles.button} 
-        activeOpacity={0.6}
-      >
-        <Text style={styles.buttonText}>
-          <Feather name="chevron-right" style={styles.buttonIcon} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de{'\n'}
+          forma fácil
         </Text>
-      </TouchableOpacity>
 
+        <Image 
+          source={wateringImg} 
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. 
+          Nós cuidamos de lembrar sempre que precisar 
+        </Text>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          activeOpacity={0.6}
+        >
+          <Feather 
+            name="chevron-right" 
+            style={styles.buttonIcon} 
+          />
+
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -48,21 +53,28 @@ export function Welcome(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     padding: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   image: {
     height: Dimensions.get('window').width * 0.7
