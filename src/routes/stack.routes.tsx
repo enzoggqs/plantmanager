7267d48f1a8746ba1402efc6e4f9b73f76231400
enzoggqs <eggqsaraiva@gmail.com>
2021-06-23 11:23:58 -0300

    Fazendo ajustes nas rotas para se adaptar ao footer de navegaçao

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,11 +5,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
 import { MyPlants } from '../pages/MyPlants';
 
 import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -39,7 +39,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
     />
 
     <stackRoutes.Screen
@@ -49,7 +49,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen
       name="MyPlants"
-      component={MyPlants}
+      component={AuthRoutes}
     />
 
   </stackRoutes.Navigator>
